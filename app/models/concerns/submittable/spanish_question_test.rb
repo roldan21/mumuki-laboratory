9eb758cdd28564f63c615a1cfd5d0926b302cd4c
giuliano64 <fgceliberti@gmail.com>
2015-10-20 23:25:52 -0300

    correcting pgSearch, test not working

--- app/models/concerns/submittable/spanish_question_test.rb
+++
@@ -1,8 +0,0 @@
-
-class Spanish_question < ActiveRecord::Base
-  include PgSearch
-  pg_search_scope :gringo_search,
-                  :against => :word,
-                  :ignoring => :accents
-end
-
--- app/models/concerns/with_search.rb
+++ app/models/concerns/with_search.rb
@@ -3,14 +3,10 @@ module WithSearch
 
   included do
     include PgSearch
-    include Spanish_question
 
 
-    pg_search_scope :full_text_search,self::INDEXED_ATTRIBUTES.deep_merge(using: {tsearch: {prefix: true}})
-
-    #pg_search_scope :full_text_search, self::INDEXED_ATTRIBUTES.deep_merge(using: {tsearch: {prefix: true}})
-    #scope :by_full_text, lambda { |q| full_text_search(q) if q.present? }
-    scope :by_full_text, lambda { |q| full_text_search(q) if q.present? }
+      pg_search_scope :full_text_search, self::INDEXED_ATTRIBUTES.deep_merge(using: {tsearch: {prefix: true },:ignoring => :accents})
+      scope :by_full_text, lambda { |q| full_text_search(q) if q.present? }
 
 
 
