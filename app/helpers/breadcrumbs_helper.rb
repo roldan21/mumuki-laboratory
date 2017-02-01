8f083833e099e795e2e978b7397ac9874594a8b9
Franco Bulgarelli <franco@mumuki.org>
2017-02-01 11:55:37 -0300

    Using new breadcrumb

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -1,16 +1,12 @@
 module BreadcrumbsHelper
   def breadcrumbs(e, extra=nil)
-    return "#{breadcrumbs(e)}<li>#{extra}</li>".html_safe if extra
+    return "#{breadcrumbs(e)}<li class=\"mu-breadcrumb-list-item\">#{extra}</li>".html_safe if extra
 
     base = link_to_path_element e
     if e.navigation_end?
-      "#{home_breadcrumb}<li>#{base}</li>".html_safe
+      "<li class=\"mu-breadcrumb-list-item\">#{base}</li>".html_safe
     else
-      "#{breadcrumbs(e.navigable_parent)} <li>#{base}</li>".html_safe
+      "#{breadcrumbs(e.navigable_parent)} <li class=\"mu-breadcrumb-list-item\">#{base}</li>".html_safe
     end
-  end
-
-  def home_breadcrumb
-    "<li>#{link_to '<span class="brand da da-mumuki"></span>'.html_safe, root_path }</li>".html_safe
   end
 end
