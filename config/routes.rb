8d028c160a1864478e7e8e2979cd37041dc754eb
Rodrigo Alfonso <rodri042@gmail.com>
2017-02-24 03:14:51 -0300

    Designing invitation view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   Mumukit::Login.configure_login_routes! self
 
   get '/join/:invitation' => 'invitations#show'
+  post '/join/:invitation' => 'invitations#join'
 
   OrganizationMapper.configure_application_routes(self) do
     root to: 'book#show'
